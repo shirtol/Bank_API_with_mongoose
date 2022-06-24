856c5a199fcdee6f0883df51cca64435516d2ce4
shirtol <shirzahavi7@gmail.com>
2022-06-24 19:35:12 +0300

    Changed heroku url

--- client/src/apis/bankApi.ts
+++ client/src/apis/bankApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL =
     process.env.NODE_ENV === "production"
-        ? "https://shirtol-bank.herokuapp.com"
+        ? "https://shirtol-bank-api-mongoose.herokuapp.com/"
         : "http://localhost:5000";
 
 export default axios.create({
